d2d210f0e6ce6f6954084aa81a79b580f4b70a7a
eslamalaa10 <54743331+eslamalaa10@users.noreply.github.com>
2021-02-11 21:04:17 +0200

    ch3 Iteration Statements

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 10)
+            {
+                WriteLine(x);
+                x++;
+            }
+
+            WriteLine("\n");
+            string password = string.Empty;
+            do
+            {
+                Write("Enter your password: ");
+                password = ReadLine();
+            }
+            while (password != "Pa$$w0rd");
+            WriteLine("Correct!\n");
+
+            for (int y = 1; y <= 10; y++)
+            {
+                WriteLine(y);
+            }
+
+            WriteLine("\n");
+            string[] names = { "Adam", "Barry", "Charlie" };
+            foreach (string name in names)
+            {
+                WriteLine($"{name} has {name.Length} characters.");
+            }
+        }
+    }
+}
