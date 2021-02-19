26634e6cfc7db5a7eee066c24739715120df8959
eslamalaa10 <54743331+eslamalaa10@users.noreply.github.com>
2021-02-19 15:46:00 +0200

    ch 3 Exercise 03

---
+++ Chapter03/Exercise03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Write("fizzbuzz, ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Write("fizz, ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Write("buzz, ");
+                }
+                else
+                {
+                    Write(i+", ");
+                }
+                 
+            }
+        }
+    }
+}
