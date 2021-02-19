ef74604abbcc6e7714fe64c0b412408586ac1ef9
eslamalaa10 <54743331+eslamalaa10@users.noreply.github.com>
2021-02-19 15:34:46 +0200

    ch 3 Exercise 02

---
+++ Chapter03/Exercise02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                    int max = 500;
+                    for (byte i = 0; i < max; i++)
+                    {
+                        WriteLine(i);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+             WriteLine(ex);   
+            }
+        }
+    }
+}
