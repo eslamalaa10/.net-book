00adff66e1eb157d2a903959fb081d44fd9a7efd
eslamalaa10 <54743331+eslamalaa10@users.noreply.github.com>
2021-02-19 16:12:30 +0200

    ch 3 Exercise 04

---
+++ Chapter03/Exercise04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 0 and 255: ");
+            String first = ReadLine();
+            Console.Write("Enter another number between 0 and 255: ");
+            String second = ReadLine();
+            try
+            {
+                int num1 = int.Parse(first);
+                int num2 = int.Parse(second);
+                Write(num1 + " divided by "+ num2 + " is " + num1/num2);
+            }
+            catch (FormatException ex)
+            {
+               WriteLine(ex.Message);
+            }
+        }
+    }
+}
